5927eb8e0e770671251d7b5ed8d16da89ebfc057
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2023-12-02 11:48:29 +0530

    Update Labels.js

--- client/src/components/Labels.js
+++ client/src/components/Labels.js
@@ -1,36 +1,4 @@
-// import React from 'react'
-// import{default as api}from '../store/apiSlice';
 
-// export default function Labels() {
-//   const{data,isFetching,isSuccess,isError}=api.useGetLabelsQuery()
-
-//   let Transaction;
-//   if(isFetching){
-// Transaction=<div>Fetching</div>;
-//   }else if(isSuccess){
-// Transaction=data.map((v,i)=><LabelComponent key= {i}data={v}></LabelComponent>)
-//   }else if(isError){
-//     Transaction=<div>Error</div>
-//   }
-//   return (
-//    <>
-//    {Transaction}
-//    </>
-//   ) 
-// }
-
-// function LabelComponent({data}){
-//   if(!data)return <></>;
-//   return(
-//     <div className='labels flex justify-between'>
-//       <div className='flex gap-2'>
-//         <div className='w-2 h-2 rounded py-3'style={{background:data.color ??'#f9c74f'}} ></div>
-//         <h3 className='text-md'>{data.type ??""}</h3>
-//       </div>
-//       <h3 className='font-bold'>{data.percent ??0}%</h3>
-//     </div>
-//   )
-//  }
 import React from 'react'
 import {default as api} from '../store/apiSlice';
  import { getLabels } from '../helper/helper';
